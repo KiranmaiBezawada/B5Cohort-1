de44520b345a9b02f3824f46078f024f5c7b852d
unknown <wave2wf02@iiht.tech>
2020-12-18 10:32:35 +0530

    abcd

--- pms-spring-web-mvc2/src/main/java/com/wellsfargo/batch5/pms/exception/PMSException.java
+++
@@ -1,8 +0,0 @@
-package com.wellsfargo.batch5.pms.exception;
-
-public class PMSException extends Exception{
-	public PMSException(String message) {
-		super(message);
-	}
-
-}
--- pms-spring-web-mvc2/src/main/webapp/pages/portfolioDashboard.jsp
+++
@@ -1,6 +0,0 @@
-<%@page import="java.time.LocalDateTime"%><html>
-<head>
-<title>PortfolioDashboard</title></head>
-<body>
-<h1>Portfolio Dashboard Page</h1>
-</body></html>
